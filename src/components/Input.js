82e8b5bad963ead009d134d636379b3a3f4db3eb
romanpoudel <romanpoudel325@gmail.com>
2022-11-25 17:21:27 +0545

    enter key pressed next

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import DoneOutlineIcon from "@mui/icons-material/DoneOutline";
 import { Tooltip } from "@mui/material";
 
@@ -9,19 +9,24 @@ function Input({ task, setTask, list, setList, clickedTask, edit, setEdit }) {
     setTask({ ...task, [name]: value });
   };
 
+  const categoryRef = useRef();
+  const dateRef = useRef();
+  const buttonRef = useRef();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // console.log(task);
     if (edit) {
       if (task.text && task.category && task.date) {
         setEdit(false);
-       setList(list.map((elem)=>{
-        if(elem.id===clickedTask){
-          return {...elem,name:task}
-        }
-        return elem;
-       }))
-        
+        setList(
+          list.map((elem) => {
+            if (elem.id === clickedTask) {
+              return { ...elem, name: task };
+            }
+            return elem;
+          })
+        );
       }
     } else {
       if (task.text && task.category && task.date) {
@@ -34,9 +39,9 @@ function Input({ task, setTask, list, setList, clickedTask, edit, setEdit }) {
     setTask({
       text: "",
       category: "",
-      date: ""
+      date: "",
     });
-    // console.log(list);
+    
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -48,6 +53,12 @@ function Input({ task, setTask, list, setList, clickedTask, edit, setEdit }) {
           onChange={handleClick}
           value={task.text}
           placeholder="Task"
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              event.preventDefault();
+              categoryRef.current.focus();
+            }
+          }}
         />
 
         <input
@@ -57,6 +68,13 @@ function Input({ task, setTask, list, setList, clickedTask, edit, setEdit }) {
           onChange={handleClick}
           value={task.category}
           placeholder="Category"
+          ref={categoryRef}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              event.preventDefault();
+              dateRef.current.focus();
+            }
+          }}
         />
         <input
           className="w-30 p-2 mx-2 border-2 outline-none rounded-lg border-gray-600"
@@ -64,13 +82,20 @@ function Input({ task, setTask, list, setList, clickedTask, edit, setEdit }) {
           name="date"
           onChange={handleClick}
           value={task.date}
+          ref={dateRef}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              event.preventDefault();
+              buttonRef.current.click();
+            }
+          }}
         />
 
         <Tooltip
           title="Add task"
           className="border-2 p-2 rounded-md border-gray-700"
         >
-          <button type="submit">
+          <button type="submit" ref={buttonRef}>
             <DoneOutlineIcon />
           </button>
         </Tooltip>
